899400c055d0eaafcf6742e5e2f819784722342a
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-04-01 20:36:16 +0530

    Create Selection_sort.java
    Sorting techniques are important and play a vital role in day-to-day activities. Selection sort is also one such technique which has a min variable that compares next elements in the array and updates the min value and at last the swapping happens.
    Time complexity is O( n^2)

---
+++ Selection_sort.java
@@ -0,0 +1,26 @@
+class Selection_sort {
+    public static void main(String args[]) {
+        int[] arr = { 1, 4, 3, 2 };
+        int n = arr.length;
+        // selection sort
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            // swap
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+
+        }
+        System.out.println();
+        // printing the whole array
+        // System.out.println(arr.values);
+    }
+}
