6fb6c67ea8996ca52babba39f050c11c5481606b
Prudhvi narayana BANDARU <93300939+Prudhvinaraya@users.noreply.github.com>
2024-04-09 22:36:12 +0530

    Update Array_List_operations.java

--- Array_List_operations.java
+++ Array_List_operations.java
@@ -1,20 +1,20 @@
-Dynamic Arrays in Java--->Arraylist
-Difference Between Arrays and Arraylist
-  ->Arrays are natural Data structres whereas ArrayList are added class
-  ->Arrays is of fixed size but we can have dynamic Array with ArrayList
-  ->Arrays can hold primitive datatypes whereas Arraylist holds the objects
-Defining an arraylist-> ArrayList <Integer> arrlst=new ArrayList<>();
-Basic operations :
-1)arrlst.add(value)-> adds the element into the arraylist
-2)arrlst.add(index,value)->adds the element in the specified location of arraylist
-3)int x=arrlst.get(index) ->will return the element in the specific index
-4)arrlst.set(index,newele) ->will return the arrlst with replacing value at the specified index
-5)arrlst.size()->returns the size of the arraylist
-6)int y=arrlst.remove(index) ->returns the object at specific location
-7)arrlst.clear()->to clear the values stored in arrlst
-8)arrlst.indexOf(value)->will return the index value of particular element
+// Dynamic Arrays in Java--->Arraylist
+// Difference Between Arrays and Arraylist
+//   ->Arrays are natural Data structres whereas ArrayList are added class
+//   ->Arrays is of fixed size but we can have dynamic Array with ArrayList
+//   ->Arrays can hold primitive datatypes whereas Arraylist holds the objects
+// Defining an arraylist-> ArrayList <Integer> arrlst=new ArrayList<>();
+// Basic operations :
+// 1)arrlst.add(value)-> adds the element into the arraylist
+// 2)arrlst.add(index,value)->adds the element in the specified location of arraylist
+// 3)int x=arrlst.get(index) ->will return the element in the specific index
+// 4)arrlst.set(index,newele) ->will return the arrlst with replacing value at the specified index
+// 5)arrlst.size()->returns the size of the arraylist
+// 6)int y=arrlst.remove(index) ->returns the object at specific location
+// 7)arrlst.clear()->to clear the values stored in arrlst
+// 8)arrlst.indexOf(value)->will return the index value of particular element
 
-here is the code that demonstrates the operations that can be done on a ArrayList
+// here is the code that demonstrates the operations that can be done on a ArrayList
 import java.util.*;
 
 class Array_List_operations {
